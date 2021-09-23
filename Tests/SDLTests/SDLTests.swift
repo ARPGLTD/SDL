c7559556f1720233dbab56ea00c3155e1f91c48c
pcbeard <pcbeard@mac.com>
2021-09-22 17:33:21 -0700

    SDL_Init() and SLD_Window tests

--- Tests/SDLTests/SDLTests.swift
+++ Tests/SDLTests/SDLTests.swift
@@ -1,10 +1,38 @@
 import XCTest
 @testable import SDL
 
+let SDL_WINDOWPOS_UNDEFINED = Int32(0x1FFF0000)
+
 final class SDLTests: XCTestCase {
     func testSpinLock() throws {
         var lock = SDL_SpinLock()
         SDL_AtomicLock(&lock)
         SDL_AtomicUnlock(&lock)
+    }
+    
+    func testSDLInit() throws {
+        let rv = SDL_Init(SDL_INIT_VIDEO)
+        XCTAssert(rv >= 0, "SDL_Init(SDL_INIT_VIDEO) failed")
+    }
+
+    
+    func testSDLWindow() throws {
+        let rv = SDL_Init(SDL_INIT_VIDEO)
+        XCTAssert(rv >= 0, "SDL_Init(SDL_INIT_VIDEO) failed")
+
+        let window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE.rawValue)
+        XCTAssertNotNil(window)
+
+        var quit = false
+        var event = SDL_Event()
+        while !quit {
+            if SDL_PollEvent(&event) == 1 {
+                if event.type == SDL_QUIT.rawValue {
+                    quit = true
+                }
+            }
+        }
+        
+        SDL_DestroyWindow(window)
     }
 }
