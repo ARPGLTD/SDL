efc8640d2a4248e5fc0d1105a066a30f85f39c55
pcbeard <pcbeard@mac.com>
2021-10-05 21:20:10 -0700

    Use working directory to compute packageURL

--- Package.swift
+++ Package.swift
@@ -367,10 +367,12 @@ sdlConfig.sourcePaths = [
 
 /// returns the file URL that contains this file, Package.swift.
 var packageURL : URL = {
+#if false
     let processInfo = ProcessInfo.processInfo
-    if let manifestPath = processInfo.environment["SWIFT_MANIFEST_PATH"] {
-        return URL(fileURLWithPath: manifestPath).deletingLastPathComponent()
+    if let manifestPath = processInfo.environment["SWIFT_PACKAGE_ROOT"] {
+        return URL(fileURLWithPath: manifestPath)
     }
+#endif
     return URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
 }()
 
@@ -402,6 +404,7 @@ func excludePaths(config : TargetConfiguration) -> [String] {
     // 1. don't exclude any explicitly included source paths.
     // 2. don't exclude any source paths that are in explicitly included directories.
     // currently this code is very sensitive to the current working directory.
+    print("packageURL = \(packageURL)")
     let targetURL = URL(fileURLWithPath: "Sources/\(config.name)", relativeTo: packageURL)
     // print("targetURL = \(targetURL.path)")
     func isDirectory(_ path : String) -> Bool {
